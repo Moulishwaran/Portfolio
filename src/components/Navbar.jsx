64234841d43076b248b13ceee17a218922ffa0e8
Moulishwaran <moulish0809@gmail.com>
2022-07-01 23:36:28 +0530

    commit change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,31 +10,33 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
+        <div className="text-5xl font-signature ml-2 color-black text-pink-600">
+          Moulishwaran
+        </div>
       </div>
       {/* menu */}
       <ul className="hidden md:flex">
-        <li>
+        <li className="hover:text-pink-600">
           <Link to="home" smooth={true} duration={400}>
             Home
           </Link>
         </li>
-        <li>
+        <li className="hover:text-pink-600">
           <Link to="about" smooth={true} duration={400}>
             About
           </Link>
         </li>
-        <li>
+        <li className="hover:text-pink-600">
           <Link to="skills" smooth={true} duration={400}>
             Skills
           </Link>
         </li>
-        <li>
+        <li className="hover:text-pink-600">
           <Link to="project" smooth={true} duration={400}>
             Project
           </Link>
         </li>
-        <li>
+        <li className="hover:text-pink-600">
           <Link to="contact" smooth={true} duration={400}>
             Contact
           </Link>
