d2ccb0d430b6f7156bd7452897962fa9a2e0c718
Moulishwaran <moulish0809@gmail.com>
2022-09-04 17:10:49 +0530

    commit change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -107,7 +107,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] sm:text-left">
             <a
               className="flex justify-between items-center  w-full text-gray-300"
-              href="https://drive.google.com/file/d/1OOjNKbvExXeB3uJJMKs2R_TyXI2bzN1u/view?usp=sharing"
+              href="https://drive.google.com/file/d/1sgSbJtWzGhiHpkYpmxz2KPau-DmBzbXL/view?usp=sharing"
               target="_blank"
             >
               Resume <BsFillPersonLinesFill size={30} />
